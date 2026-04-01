8c22b8eb15ba553dd4cb46b6f74626cd68be71de
Yousef <yousef.ah@outlook.com>
2026-04-01 12:23:06 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -9,11 +9,10 @@ class Socket():
 def main():
     server_socket = socket.create_server(('localhost', 6379), reuse_port=True)
     
-
     while True: 
         connection, _ = server_socket.accept() 
         connection.recv(1024)
-        connection.sendall(b"+PONG\r\n")
+        connection.sendall(b'+PONG\r\n')
 
 
 if __name__ == '__main__':
