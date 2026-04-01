71ed2df6b96cdfabc76863df69393ee605c8a50c
Yousef <yousef.ah@outlook.com>
2026-04-01 15:02:04 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,16 +4,18 @@ import asyncio
 
 async def handle_client(reader, writer):
      while True:
-        response = reader.read()  # Receive data as bytes object from the socket
+        response = reader.read(1024)  # Receive data as bytes object from the socket
+        await response
         writer.write(b'+PONG\r\n')  # Send data to the socket
         if response == b'': # Close the connection and exit the loop if the response is empty
-            response.close()
+            writer.close()
             break
 
 
 
 async def main():
     server_socket = asyncio.start_server(handle_client, 'localhost', 6379)
+    await server_socket
 
 
 if __name__ == '__main__':
