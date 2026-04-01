8a25f65f37a73620ceb9cdda3f05cd74ed5ad177
Yousef <yousef.ah@outlook.com>
2026-04-01 12:32:56 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,18 +1,15 @@
 import socket  # noqa: F401
 
 
-class Socket(): 
-    def _init__(self): 
-        self.socket = socket.socket()
-
-
 def main():
     server_socket = socket.create_server(('localhost', 6379), reuse_port=True)
-    
+    response = None
+
     while True: 
-        connection, _ = server_socket.accept() 
-        connection.recv(1024)
-        connection.sendall(b'+PONG\r\n')
+        while response != "b''": 
+            connection, _ = server_socket.accept() 
+            connection.recv(1024)
+            connection.sendall(b'+PONG\r\n')
 
 
 if __name__ == '__main__':
