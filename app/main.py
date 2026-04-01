f7f5639cd4ad06d766674430e178696ab9cfe9cb
Yousef <yousef.ah@outlook.com>
2026-04-01 11:11:38 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,7 +4,11 @@ import socket  # noqa: F401
 def main():
     server_socket = socket.create_server(('localhost', 6379), reuse_port=True)
     connection, _ = server_socket.accept() # wait for client
-    connection.sendall(b"+PONG\r\n")
+    
+    while True: 
+        line = input('$ ')
+        connection.sendall(b"+PONG\r\n")
+
 
 
 if __name__ == '__main__':
