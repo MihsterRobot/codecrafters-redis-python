8da6744878a0cd7b6d2f0a816639a08af400a483
Yousef <yousef.ah@outlook.com>
2026-04-01 14:08:47 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,13 +5,14 @@ def main():
     server_socket = socket.create_server(('localhost', 6379), reuse_port=True)
     response = None
 
-    while True: 
-        while response != b'': 
-            connection, _ = server_socket.accept()  # Return a new socket representing the client connection
-            response = connection.recv(1024)  # Receive data as bytes object from socket
+    while True:
+        connection, _ = server_socket.accept()  # Establish a connection with the client
+        while True: 
+            response = connection.recv(1024)  # Receive data as bytes object from the socket
             connection.sendall(b'+PONG\r\n')  # Send data to the socket
-        connection.close() 
-        response = None
+            if not response: # Exit the loop if the response if empty
+                break
+        connection.close()
 
 
 if __name__ == '__main__':
