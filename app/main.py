e7b98a0db5e473c3be723ef6815bd17d8fc421ee
Yousef <yousef.ah@outlook.com>
2026-04-01 12:20:49 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,11 +1,17 @@
 import socket  # noqa: F401
 
 
+class Socket(): 
+    def _init__(self): 
+        self.socket = socket.socket()
+
+
 def main():
     server_socket = socket.create_server(('localhost', 6379), reuse_port=True)
-    connection, _ = server_socket.accept() # wait for client
     
+
     while True: 
+        connection, _ = server_socket.accept() 
         connection.recv(1024)
         connection.sendall(b"+PONG\r\n")
 
