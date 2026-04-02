2b004d63917b4ec44c06eb5dee81f80ac99de57f
Yousef <yousef.ah@outlook.com>
2026-04-02 15:37:51 +0100

    codecrafters submit [skip ci]

--- app/commands.py
+++ app/commands.py
@@ -18,7 +18,7 @@ def run_set(args: str) -> bytes:
 
 
 def run_get(args: str) -> bytes:
-    value = STORE[args[0]]
+    value = STORE.get(args)
     if value is None: 
         return b'$-1\r\n'
     return f'{len(value)}\r\n{value}\r\n'.encode()
