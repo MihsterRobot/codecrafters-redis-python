7288396bffca9719fe2809a89c88be7812c3c5a3
Yousef <yousef.ah@outlook.com>
2026-04-04 08:34:38 +0100

    codecrafters submit [skip ci]

--- app/resp.py
+++ app/resp.py
@@ -3,7 +3,7 @@ def parse_resp(data: bytes) -> tuple[str, list[str]]:
 
     # Remove terminators to isolate the data and their size identifiers.
     tokens = data.split(crlf)
-    print(tokens)
+    print('TOKENS:', tokens)
     num_elements = int(tokens[0][1:])
     stop = num_elements * 2 + 1
     cmd_parts = []
@@ -12,6 +12,8 @@ def parse_resp(data: bytes) -> tuple[str, list[str]]:
         cmd_parts.append(tokens[i].decode())
 
     cmd_name = cmd_parts[0]
+    print('CMD_NAME:', cmd_name)
     args = cmd_parts[1:] if len(cmd_parts) > 1 else []
+    print('ARGS:', args)
 
     return cmd_name, args
