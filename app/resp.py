31a888634bb61b4c39858323179fb280bfd05e78
Yousef <yousef.ah@outlook.com>
2026-04-04 09:32:33 +0100

    codecrafters submit [skip ci]

--- app/resp.py
+++ app/resp.py
@@ -4,7 +4,11 @@ def parse_resp(data: bytes) -> tuple[str, list[str]]:
     # Remove terminators to isolate the data and their size identifiers.
     tokens = data.split(crlf)
     print('TOKENS:', tokens)
+
+    # Strip the leading '*' from the array marker and convert the element count to an int.
     num_elements = int(tokens[0][1:])
+    print('NUM_ELEMENTS:', num_elements)
+
     stop = num_elements * 2 + 1
     cmd_parts = []
 
