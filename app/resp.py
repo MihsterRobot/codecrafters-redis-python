40d6b9e5430740ae50fd98cee3fbdf72aa155808
Yousef <yousef.ah@outlook.com>
2026-04-04 09:34:28 +0100

    codecrafters submit [skip ci]

--- app/resp.py
+++ app/resp.py
@@ -3,11 +3,11 @@ def parse_resp(data: bytes) -> tuple[str, list[str]]:
 
     # Remove terminators to isolate the data and their size identifiers.
     tokens = data.split(crlf)
-    print('TOKENS:', tokens)
+    # print('TOKENS:', tokens)  # Debug
 
     # Strip the leading '*' from the array marker and convert the element count to an int.
     num_elements = int(tokens[0][1:])
-    print('NUM_ELEMENTS:', num_elements)
+    # print('NUM_ELEMENTS:', num_elements)  # Debug
 
     stop = num_elements * 2 + 1
     cmd_parts = []
@@ -16,8 +16,8 @@ def parse_resp(data: bytes) -> tuple[str, list[str]]:
         cmd_parts.append(tokens[i].decode())
 
     cmd_name = cmd_parts[0]
-    print('CMD_NAME:', cmd_name)
+    # print('CMD_NAME:', cmd_name)  # Debug
     args = cmd_parts[1:] if len(cmd_parts) > 1 else []
-    print('ARGS:', args)
+    # print('ARGS:', args)  # Debug
 
     return cmd_name, args
