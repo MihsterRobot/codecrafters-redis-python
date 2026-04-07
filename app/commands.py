06a81812ce4a00cec044f542918d80191720d9e8
Yousef <yousef.ah@outlook.com>
2026-04-07 15:30:22 +0100

    codecrafters submit [skip ci]

--- app/commands.py
+++ app/commands.py
@@ -19,6 +19,20 @@ def run_echo(args: list[str]) -> bytes:
     return f'${len(args[0])}\r\n{args[0]}\r\n'.encode()
 
 
+def run_type(args: list[str]) -> bytes:
+    key = args[0]
+    entry = STORE.get(key)
+    lst = entry.value if entry is not None else []
+
+    if not lst: 
+        return b'+none\r\n'
+
+    typ = type(lst)
+
+    return f'+{typ}\r\n'.encode()
+    
+
+
 def run_set(args: list[str]) -> bytes:
     key = args[0]
     expiry_time = None
@@ -61,42 +75,6 @@ def run_rpush(args: list[str]) -> bytes:
     return f':{len(lst)}\r\n'.encode()
 
 
-def run_lrange(args: list[str]) -> bytes:
-    key = args[0]
-    entry = STORE.get(key)
-
-    # Return an empty array if the key doesn't exist.
-    if entry is None:
-        return b'*0\r\n'  
-    
-    lst = entry.value
-    lst_size = len(lst)
-
-    # Convert negative indexes to their positive equivalents using the list length as the offset.
-    # Clamp any out-of-bounds negative index to 0.
-    start, stop = int(args[1]), int(args[2])
-    start = max(0, lst_size + start if start < 0 else start)
-    stop = max(0, lst_size + stop if stop < 0 else stop)
-
-    # Clamp stop to the last valid index if it exceeds the list length.
-    if stop >= lst_size: 
-        stop = lst_size - 1
-
-    # Return an empty array if start is out of bounds or greater than stop.
-    if start >= lst_size or start > stop: 
-        return b'*0\r\n'  
-
-    sliced = lst[start:stop+1]
-    resp_lst = [f'*{len(sliced)}\r\n']
-
-    for elmt in sliced:
-        resp_lst.append(f'${len(elmt)}\r\n')
-        resp_lst.append(f'{elmt}\r\n')
-    
-    # Join the RESP parts into a single continuous bytes object before sending over the socket.
-    return ''.join(resp_lst).encode()
-
-
 def run_lpush(args: list[str]) -> bytes: 
     key, elements = args[0], args[1:]
     entry = STORE.get(key)
@@ -106,17 +84,6 @@ def run_lpush(args: list[str]) -> bytes:
     lst = elements[::-1] + lst  
     STORE[key] = StoreEntry(value=lst, expiry_time=None)
 
-    return f':{len(lst)}\r\n'.encode()
-
-
-def run_llen(args: list[str]) -> bytes:
-    key = args[0]
-    entry = STORE.get(key)
-    lst = entry.value if entry is not None else []
-
-    if not lst:
-        return b':0\r\n'
-    
     return f':{len(lst)}\r\n'.encode()
 
 
@@ -189,15 +156,63 @@ async def run_blpop(args: list[str]) -> bytes:
     return f'*2\r\n${len(key)}\r\n{key}\r\n${len(elmt)}\r\n{elmt}\r\n'.encode()
 
 
+def run_lrange(args: list[str]) -> bytes:
+    key = args[0]
+    entry = STORE.get(key)
+
+    # Return an empty array if the key doesn't exist.
+    if entry is None:
+        return b'*0\r\n'  
+    
+    lst = entry.value
+    lst_size = len(lst)
+
+    # Convert negative indexes to their positive equivalents using the list length as the offset.
+    # Clamp any out-of-bounds negative index to 0.
+    start, stop = int(args[1]), int(args[2])
+    start = max(0, lst_size + start if start < 0 else start)
+    stop = max(0, lst_size + stop if stop < 0 else stop)
+
+    # Clamp stop to the last valid index if it exceeds the list length.
+    if stop >= lst_size: 
+        stop = lst_size - 1
+
+    # Return an empty array if start is out of bounds or greater than stop.
+    if start >= lst_size or start > stop: 
+        return b'*0\r\n'  
+
+    sliced = lst[start:stop+1]
+    resp_lst = [f'*{len(sliced)}\r\n']
+
+    for elmt in sliced:
+        resp_lst.append(f'${len(elmt)}\r\n')
+        resp_lst.append(f'{elmt}\r\n')
+    
+    # Join the RESP parts into a single continuous bytes object before sending over the socket.
+    return ''.join(resp_lst).encode()
+
+
+def run_llen(args: list[str]) -> bytes:
+    key = args[0]
+    entry = STORE.get(key)
+    lst = entry.value if entry is not None else []
+
+    if not lst:
+        return b':0\r\n'
+    
+    return f':{len(lst)}\r\n'.encode()
+
+
 COMMANDS = {
     'PING': run_ping,
     'ECHO': run_echo,
+    'TYPE': run_type,
     'SET': run_set,
     'GET': run_get,
     'RPUSH': run_rpush,
-    'LRANGE': run_lrange,
     'LPUSH': run_lpush,
-    'LLEN': run_llen,
     'LPOP': run_lpop,
-    'BLPOP': run_blpop
+    'BLPOP': run_blpop,
+    'LRANGE': run_lrange,
+    'LLEN': run_llen
 }
