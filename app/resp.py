04a772bff5d8bbaba4504aac3cc8622423d0139c
Yousef <yousef.ah@outlook.com>
2026-04-12 08:21:29 +0100

    codecrafters submit [skip ci]

--- app/resp.py
+++ app/resp.py
@@ -7,7 +7,7 @@ def parse_resp(data: bytes) -> tuple[str, list[str]]:
 
     stop = num_elements * 2 + 1
     cmd_parts = []
-    for i in range(2, stop, 2):
+    for i in range(2, stop, 2):  # Converting all args to uppercase incorrectly returns array args in uppercase.
         # Decode each token and convert to uppercase for case-insensitive command handling.
         cmd_parts.append(tokens[i].decode())
 
