638d2f3af475acb862fcae21156e57de2860f497
Yousef <yousef.ah@outlook.com>
2026-04-12 08:12:50 +0100

    codecrafters submit [skip ci]

--- app/resp.py
+++ app/resp.py
@@ -8,7 +8,7 @@ def parse_resp(data: bytes) -> tuple[str, list[str]]:
     stop = num_elements * 2 + 1
     cmd_parts = []
     for i in range(2, stop, 2):
-        cmd_parts.append(tokens[i].decode())
+        cmd_parts.append(tokens[i].decode().upper())
 
     cmd_name = cmd_parts[0]
     args = cmd_parts[1:] if len(cmd_parts) > 1 else []
