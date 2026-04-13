6c7bd42e5a068ee56fe08e4e051cc670fb88a4a3
Yousef <yousef.ah@outlook.com>
2026-04-13 12:24:32 +0100

    codecrafters submit [skip ci]

--- app/commands.py
+++ app/commands.py
@@ -428,12 +428,11 @@ def run_incr(args: list[str]) -> bytes:
     result = ''
 
     if store_entry is None:
-        result = '1'
+        result = 1
     else:
         result = int(store_entry.value) + 1
-        result = str(result)
 
-    STORE[key] = StoreEntry(value=result, expiry_time=None, redis_type='string')
+    STORE[key] = StoreEntry(value=str(result), expiry_time=None, redis_type='string')
 
     return f':{result}\r\n'.encode()
 
