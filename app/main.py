693b3ec0cea275e6204bbec41889919578033b4d
Yousef <yousef.ah@outlook.com>
2026-04-15 09:32:32 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -91,9 +91,8 @@ async def handle_client(reader: asyncio.StreamReader, writer: asyncio.StreamWrit
             writer.write(result)
 
             if cmd_name == 'PSYNC': 
-                rdb_file = '524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2'.encode()
-                size = len(rdb_file)
-                writer.write(f'${size}\r\n{rdb_file}'.encode())
+                rdb_file = bytes.fromhex('524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2')
+                writer.write(f'${len(rdb_file)}\r\n'.encode() + rdb_file)
 
 
 async def main() -> None:
